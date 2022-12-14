50928c425b9a2230c8d9a8aed92b4f10ab6fe581
TheVirtualPhysicist <64352871+TheVirtualPhysicist@users.noreply.github.com>
2022-12-14 16:01:19 +0100

    Add files via upload

---
+++ Strategy.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import pandas_ta as pa
+from yfinance import download
+from datetime import datetime, timedelta
+from xgboost import XGBClassifier
+from cryptography.fernet import Fernet
+import os
+
+class Strategy:
+	def __init__(self, exchange):
+		self.df = -1
+		self.exchange = exchange+"-EUR"
+		self.interval = 5 # minutes
+		self.invest = 0.99
+
+		self.loadModel()
+
+		self.strategy = "-"
+		# parametri
+		# self.tassa = 0.01
+		# self.moltiplicatore = 1
+
+	def loadModel(self):
+		fernet = Fernet(os.environ['FERNET'])
+		f = open("cmodel.json","rb")
+		text = f.read()
+		f.close()
+		f = open("model.json","wb")
+		f.write(fernet.decrypt(text))
+		f.close()
+		self.model = XGBClassifier()
+		self.model.load_model("model.json")
+
+
+	def checkEnter(self):
+		self.updateData()
+		prediction = int(self.model.predict(self.df.iloc[-1:]))
+		if prediction == 1:
+			self.strategy = "long"
+		sl = 0
+		tp = 0
+		return self.strategy, sl, tp
+
+	def actOnOpenPosition(self):
+		return [False,""] # trail stop to be implemented
+
+	def check_exit(self):
+		# this is actually something not necessary, since
+		# stopCalls will be automated. But is good to have
+		# to both add security, and also for a possible
+		# future generalization
+		"""if self.strategy != "-":
+			if "short" in self.strategy:
+				if self.stopCallShort():
+					self.strategy = "-"
+			elif "short" not in self.strategy:
+				if self.stopCallLong():
+					self.strategy = "-"
+		return self.strategy == "-"
+		"""
+		return
+
+	def getRawData(self): # CONTROLLARE CHE SIA AGGIORNATO
+		STEP = 50
+		interval = "5m"
+
+		self.df = download(self.exchange, start=(datetime.now()-timedelta(minutes=STEP)).date(), end=datetime.now(), interval=interval, auto_adjust=False, prepost=False)
+		self.df.index.names = ["Gmt time"]
+		self.df = self.df[['Open','High','Low','Close','Volume']]
+
+	def updateData(self):
+		self.getRawData()
+		# Analyzing data
+		self.df["RSI"] = pa.rsi(self.df.Close, length=16)
+		self.df["CCI"] = pa.cci(self.df.High, self.df.Low, self.df.Close, length=16)
+		self.df["AO"] = pa.ao(self.df.High, self.df.Low)
+		self.df["MOM"] = pa.mom(self.df.Close, length=16)
+		a = pa.macd(self.df.Close)
+		self.df = self.df.join(a)
+		self.df["ATR"] = pa.atr(self.df.High, self.df.Low, self.df.Close, length=16)
+		self.df["BOP"] = pa.bop(self.df.Open, self.df.High, self.df.Low, self.df.Close, length=16)
+		self.df["RVI"] = pa.rvi(self.df.Close)
+		a = pa.dm(self.df.High, self.df.Low, length=16)
+		self.df = self.df.join(a)
+		a = pa.stoch(self.df.High, self.df.Low, self.df.Close)
+		self.df = self.df.join(a)
+		a = pa.stochrsi(self.df.Close, length=16)
+		self.df = self.df.join(a)
+		self.df["WPR"] = pa.willr(self.df.High, self.df.Low, self.df.Close, length=16)
+		attributes = ['RSI', 'CCI', 'AO', 'MOM', 'MACD_12_26_9', 'MACDh_12_26_9', 'MACDs_12_26_9', 'ATR','BOP', 'RVI', 'DMP_16', 'DMN_16', 'STOCHk_14_3_3', 'STOCHd_14_3_3','STOCHRSIk_16_14_3_3', 'STOCHRSId_16_14_3_3', 'WPR']
+		self.df = self.df[attributes]
+
+	def get_current_state(self):
+		"""r = f"Close: {self.df.iloc[-1]['Close']} | "
+		r += f"macd_diff: {self.df.iloc[-1]['macd_diff']} | "
+		r += f"rocEMA100: {self.df.iloc[-1]['rocEMA100']} | "
+		r += f"bollinger_pband: {self.df.iloc[-1]['bollinger_pband']} | "
+		r += f"bollinger_wband: {self.df.iloc[-1]['bollinger_wband']}"
+		return r
+		"""
+		return
+
+
+	def getData(asset):
+		SPAN = 500 # < 720
+		i = 0
+		while i<10:
+			data = download(f"{asset}-EUR", start=datetime.now()-timedelta(hours=500), end=datetime.now(), interval="1h", auto_adjust=False, prepost=False).astype(float).sort_index()
+			if data.iloc[-1]['Open'] == data.iloc[-1]['Close']:
+				break
+			i += 1
+			sleep(10)
+		return data.iloc[:-1]
---
+++ service.py
@@ -0,0 +1,101 @@
+# Service that manage the discord bot
+# Sonoda 10/12/2022
+
+import traceback
+import os
+import Bot as BotLib
+from webserver import keep_alive
+
+
+bot = BotLib.Bot()
+
+@bot.client.event
+async def on_ready():
+	for guild in bot.client.guilds:
+		print(
+			f'{bot.client.user} is connected to the following guild:\n'
+			f'{guild.name}(id: {guild.id})'
+		)
+
+	await bot.startConnectionMessage()
+	while True:
+		# running mode
+		if bot.SESSION=="run":
+			try:
+				await bot.runLoop()
+			except Exception as e:
+				print(e)
+				print(traceback.format_exc())
+				await bot.crashMessage(traceback.format_exc())
+		# sleep mode
+		elif bot.SESSION=="sleep":
+			try:
+				await asyncio.sleep(100)
+			except Exception as e:
+				print(e)
+				await bot.crashMessage(e)
+		# end program
+		elif bot.SESSION=="exit":
+			break
+
+
+@bot.client.event
+async def on_message(message):
+	if message.author == bot.client.user:
+		return
+
+	# da rendere migliore
+	return
+	# dividere comandi tell and do <--
+
+	if message.channel.id==azioniCH:
+		# Temporary offline-mode
+		if message.content=="shutdown" or message.content=="s":
+			bot.SESSION = not bot.SESSION
+			await message.channel.send(f"Execution is now set to {bot.SESSION}")
+		# Terminating program
+		elif message.content=="ss":
+			bot.SESSION = -1
+			await bot.client.close()
+		# Help command
+		elif message.content=="help" or message.content=="h":
+			await message.channel.send(f"help-h\nversion-v\nshutdown/execute-s\nbalance-b\nstate-c\nforce buy 0\nforce sell\nbook\nenter/exit e")
+		# Version command
+		elif message.content=="version" or message.content=="v":
+			await message.channel.send(f"B2.0.0")
+		# Modify state, enter->exit, or exit->enter
+		elif message.content=="enter" or message.content=="exit" or message.content=="e":
+			Agent.dentro = not Agent.dentro
+			await message.channel.send(f"Stato corrente aggiornato: dentro={Agent.dentro}") # <-
+		# Modify entry price value
+		elif message.content=="entrata":
+			Agent.entrata = message.content.split(" ")[1]
+		# Get balance command
+		elif message.content=="balance" or message.content=="b":
+			await message.channel.send(Agent.get_total_balance())
+		# Get flags for state
+		elif message.content=="state" or message.content=="c": # c di current state
+			r = Agent.get_current_state(get_data(Agent.currentName))
+			await message.channel.send(r)
+		# Force a buy command
+		elif "force enter" in message.content:
+			if len(message.content.split(" "))==3:
+				flag, r = Agent.buy(datetime.fromtimestamp(time()).strftime("%H:%M:%S"),get_data(Agent.currentName),forced=True)
+				if flag:
+					await bot.client.get_channel(transazioniCH).send(r)
+			# Buy short
+			elif len(message.content.split(" "))==4:
+				flag, r = Agent.buy(datetime.fromtimestamp(time()).strftime("%H:%M:%S"),get_data(Agent.currentName),forced=True,short=True)
+				if flag:
+					await bot.client.get_channel(transazioniCH).send(r)
+		# Force a sell command
+		elif message.content=="force exit":
+			flag, r = Agent.sell(datetime.fromtimestamp(time()).strftime("%H:%M:%S"),get_data(Agent.currentName),True)
+			if flag:
+				await bot.client.get_channel(transazioniCH).send(r)
+
+
+#Bottom of Main.py
+
+keep_alive()
+bot.client.run(os.environ['DISCORD_TOKEN'])
